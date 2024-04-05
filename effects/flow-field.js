d3fc4fc87390d01aa24bfcd01e30713d0b3ee760
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-04-05 22:09:43 +0000

    automatic push

--- effects/flow-field.js
+++ effects/flow-field.js
@@ -125,7 +125,7 @@ class Particle {
 Setup
 --------------------*/
 function setup() {
-  createCanvas(windowWidth, windowHeight)
+  createCanvas(displayWidth, windowHeight)
   for (let i = 0; i < opt.particles; i++) {
     Particles.push(new Particle(Math.random() * width, Math.random() * height))
   }
