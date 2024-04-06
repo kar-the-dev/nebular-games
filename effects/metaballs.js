b6ff56bad2a9e8f676087618ae90b4c1010b0be4
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-04-06 15:41:30 +0000

    automatic push

--- effects/metaballs.js
+++ effects/metaballs.js
@@ -24,9 +24,9 @@ var vertexShaderSrc = `
 attribute vec2 position;
 
 void main() {
-// position specifies only x and y.
-// We set z to be 0.0, and w to be 1.0
-gl_Position = vec4(position, 0.0, 1.0);
+  // position specifies only x and y.
+  // We set z to be 0.0, and w to be 1.0
+  gl_Position = vec4(position, 0.0, 1.0);
 }
 `;
 
