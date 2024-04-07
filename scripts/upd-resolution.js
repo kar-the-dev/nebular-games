efd1d4f46850692d636354701ee048a1f90170f7
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-04-07 20:22:32 +0000

    automatic push

--- scripts/upd-resolution.js
+++ scripts/upd-resolution.js
@@ -1,6 +1,6 @@
 var root = document.documentElement;
 
-requestAnimationFrame() => {
+requestAnimationFrame(() => {
   root.style.setProperty('--screen-w', window.screen.width*1.5);
   root.style.setProperty('--screen-h', window.screen.height*1.5);
-})
+});
