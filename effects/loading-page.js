32a0b4a2591bb75be88e0f3674a666b7050fe331
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-04-07 18:16:11 +0000

    automatic push

--- effects/loading-page.js
+++ effects/loading-page.js
@@ -1,5 +1,7 @@
 document.onreadystatechange(() => {
     if(document.readyState !== "complete") {
         document.querySelector(".spinner").style.visibility = "visible"
+    } else {
+        
     }
 })
