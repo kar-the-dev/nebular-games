850867533a7bfe7e533e9c8559d1f815770b8b4d
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-04-07 20:22:56 +0000

    automatic push

--- scripts/upd-resolution.js
+++ scripts/upd-resolution.js
@@ -1,6 +1,6 @@
 var root = document.documentElement;
 
-() => {
+function loop () {
   root.style.setProperty('--screen-w', window.screen.width*1.5);
   root.style.setProperty('--screen-h', window.screen.height*1.5);
-});
+}
