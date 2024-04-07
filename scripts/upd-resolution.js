cb76b1d1791bf5224d78c2ba9459b6b0b56c28bb
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-04-07 20:26:44 +0000

    automatic push

--- scripts/upd-resolution.js
+++ scripts/upd-resolution.js
@@ -1,6 +1,6 @@
 function loop() {
   $(':root').css("--screen-w", $(window).width());
-  $(':root').css("--screen-h", $(window).height());
+  $(':root').css("--screen-h", $(window).height() * 2);
   requestAnimationFrame(loop);
 }
 
