38cc014d831a979e6a7232250566e9328e1d5462
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-04-07 20:21:14 +0000

    automatic push

--- scripts/upd-resolution.js
+++ scripts/upd-resolution.js
@@ -3,4 +3,5 @@ var root = document.documentElement;
 document.addEventListener('resize', () => {
   root.style.setProperty('--screen-w', window.screen.width*1.5);
   root.style.setProperty('--screen-h', window.screen.height*1.5);
+  alert('resize?')
 })
