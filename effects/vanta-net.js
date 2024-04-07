5b5999c566c03fc68a3e098d8454845b0efb4ac2
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-04-07 20:55:21 +0000

    automatic push

--- effects/vanta-net.js
+++ effects/vanta-net.js
@@ -1,1 +1,10 @@
-VANTA.WAVES('body');
+VANTA.NET({
+    el: "#your-element-selector",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
