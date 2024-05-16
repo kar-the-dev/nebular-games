197c19061f1824a2523cf7ea66260e83fc4ba5ab
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-05-16 02:14:23 +0000

    automatic push

--- scripts/home.js
+++ scripts/home.js
@@ -2,186 +2,186 @@
 // https://github.com/cubedhuang/cubedhuang.github.io/blob/master/home.js (lines 31-215)
 // licensed under GNU Public License v3.0
 
-	const canvas = document.getElementById("hero-canvas");
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	const ctx = canvas.getContext("2d");
-	const particles = [];
+const canvas = document.getElementById("hero-canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d");
+const particles = [];
 
-	const mouse = {
-		ex: canvas.width / 2,
-		ey: canvas.height / 2,
-		x: canvas.width / 2,
-		y: canvas.height / 2,
-		in: false,
-		inTime: 0,
-		down: false,
-		downTime: 0
-	};
+const mouse = {
+  ex: canvas.width / 2,
+  ey: canvas.height / 2,
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  in: false,
+  inTime: 0,
+  down: false,
+  downTime: 0,
+};
 
-	window.onresize = e => {
-		canvas.width = document.body.clientWidth;
-		canvas.height = window.innerHeight;
-	};
+window.onresize = (e) => {
+  canvas.width = document.body.clientWidth;
+  canvas.height = window.innerHeight;
+};
 
-	window.onmousemove = window.ontouchmove = e => {
-		mouse.ex = e.clientX ?? e.touches[0].clientX;
-		mouse.ey = e.clientY ?? e.touches[0].clientY;
-		mouse.inTime = Date.now();
-		if (!mouse.in) {
-			mouse.in = true;
-		}
-	};
+window.onmousemove = window.ontouchmove = (e) => {
+  mouse.ex = e.clientX ?? e.touches[0].clientX;
+  mouse.ey = e.clientY ?? e.touches[0].clientY;
+  mouse.inTime = Date.now();
+  if (!mouse.in) {
+    mouse.in = true;
+  }
+};
 
-	window.onmouseout = e => {
-		mouse.in = false;
-	};
+window.onmouseout = (e) => {
+  mouse.in = false;
+};
 
-	window.onmousedown = window.ontouchstart = e => {
-		mouse.down = !e.touches;
-		mouse.in = true;
-		mouse.ex = e.clientX ?? e.touches[0].clientX;
-		mouse.ey = e.clientY ?? e.touches[0].clientY;
-	};
+window.onmousedown = window.ontouchstart = (e) => {
+  mouse.down = !e.touches;
+  mouse.in = true;
+  mouse.ex = e.clientX ?? e.touches[0].clientX;
+  mouse.ey = e.clientY ?? e.touches[0].clientY;
+};
 
-	window.onmouseup = window.ontouchend = e => {
-		mouse.down = false;
-		mouse.downTime = Date.now();
-	};
+window.onmouseup = window.ontouchend = (e) => {
+  mouse.down = false;
+  mouse.downTime = Date.now();
+};
 
-	const range = (a, b) => Math.random() * (b - a) + a;
-	const rint = (a, b) => Math.floor(Math.random() * (b - a + 1) + a);
+const range = (a, b) => Math.random() * (b - a) + a;
+const rint = (a, b) => Math.floor(Math.random() * (b - a + 1) + a);
 
-	let hoverDiv = 8;
-	let distMult = 0;
+let hoverDiv = 8;
+let distMult = 0;
 
-	class Particle {
-		static colors = ["#0984e360", "#6c5ce760"];
+class Particle {
+  static colors = ["#0984e360", "#6c5ce760"];
 
-		constructor() {
-			this.x = range(0, canvas.width);
-			this.y = range(0, canvas.height);
+  constructor() {
+    this.x = range(0, canvas.width);
+    this.y = range(0, canvas.height);
 
-			let r = range(0, Math.PI * 2);
-			this.vx = Math.cos(r) / range(2, 4);
-			this.vy = Math.sin(r) / range(2, 4);
+    let r = range(0, Math.PI * 2);
+    this.vx = Math.cos(r) / range(2, 4);
+    this.vy = Math.sin(r) / range(2, 4);
 
-			this.size = range(2, 4);
-			this.gsize = this.size; // goal size
-			this.asize = this.size; // actual size
+    this.size = range(2, 4);
+    this.gsize = this.size; // goal size
+    this.asize = this.size; // actual size
 
-			this.color = Particle.colors[rint(0, Particle.colors.length - 1)];
+    this.color = Particle.colors[rint(0, Particle.colors.length - 1)];
 
-			this.num = rint(4, 7);
-			this.vr = range(-0.05, 0.05);
-			this.rotation = range(0, Math.PI * 2);
+    this.num = rint(4, 7);
+    this.vr = range(-0.05, 0.05);
+    this.rotation = range(0, Math.PI * 2);
 
-			this.points = [];
-			for (let i = 0; i < this.num; i++) {
-				const r = Math.PI * ((i / this.num) * 2) + this.rotation;
-				this.points.push([Math.cos(r), Math.sin(r)]);
-			}
-		}
+    this.points = [];
+    for (let i = 0; i < this.num; i++) {
+      const r = Math.PI * ((i / this.num) * 2) + this.rotation;
+      this.points.push([Math.cos(r), Math.sin(r)]);
+    }
+  }
 
-		dist() {
-			if (!mouse.in) return 10000;
-			const dx = this.x - mouse.x;
-			const dy = this.y - mouse.y;
-			return Math.hypot(dx, dy);
-		}
+  dist() {
+    if (!mouse.in) return 10000;
+    const dx = this.x - mouse.x;
+    const dy = this.y - mouse.y;
+    return Math.hypot(dx, dy);
+  }
 
-		draw() {
-			this.rotation += this.vr;
+  draw() {
+    this.rotation += this.vr;
 
-			const alpha = (this.asize - this.size) / this.size / hoverDiv;
-			if (alpha >= 0.01) {
-				ctx.beginPath();
-				ctx.moveTo(mouse.x, mouse.y);
-				ctx.lineTo(this.x, this.y);
-				ctx.strokeStyle = `rgba(255, 255, 255, ${alpha})`;
-				ctx.stroke();
-			}
+    const alpha = (this.asize - this.size) / this.size / hoverDiv;
+    if (alpha >= 0.01) {
+      ctx.beginPath();
+      ctx.moveTo(mouse.x, mouse.y);
+      ctx.lineTo(this.x, this.y);
+      ctx.strokeStyle = `rgba(255, 255, 255, ${alpha})`;
+      ctx.stroke();
+    }
 
-			ctx.translate(this.x, this.y);
-			ctx.rotate(this.rotation);
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.rotation);
 
-			ctx.beginPath();
-			for (const m of this.points) {
-				ctx.lineTo(this.asize * m[0], this.asize * m[1]);
-			}
-			ctx.closePath();
-			ctx.fillStyle = this.color;
-			ctx.fill();
+    ctx.beginPath();
+    for (const m of this.points) {
+      ctx.lineTo(this.asize * m[0], this.asize * m[1]);
+    }
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
 
-			ctx.setTransform(1, 0, 0, 1, 0, 0);
-		}
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+  }
 
-		update() {
-			this.x += this.vx;
-			this.y += this.vy;
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
 
-			const s2 = this.size * 2;
-			let edge = false;
-			if (this.x > canvas.width + s2) {
-				this.x = -s2;
-				edge = true;
-			} else if (this.x < -s2) {
-				this.x = canvas.width + s2;
-				edge = true;
-			}
-			if (this.y > canvas.height + s2) {
-				this.y = -s2;
-				edge = true;
-			} else if (this.y < -s2) {
-				this.y = canvas.height + s2;
-				edge = true;
-			}
-			if (edge) {
-				this.asize = this.size;
-			}
+    const s2 = this.size * 2;
+    let edge = false;
+    if (this.x > canvas.width + s2) {
+      this.x = -s2;
+      edge = true;
+    } else if (this.x < -s2) {
+      this.x = canvas.width + s2;
+      edge = true;
+    }
+    if (this.y > canvas.height + s2) {
+      this.y = -s2;
+      edge = true;
+    } else if (this.y < -s2) {
+      this.y = canvas.height + s2;
+      edge = true;
+    }
+    if (edge) {
+      this.asize = this.size;
+    }
 
-			// goal size
-			const gsize = Math.max(
-				Math.min((distMult * this.size) / this.dist(), this.size * 2),
-				this.size
-			);
-			this.asize += (gsize - this.asize) / 4;
-		}
-	}
+    // goal size
+    const gsize = Math.max(
+      Math.min((distMult * this.size) / this.dist(), this.size * 2),
+      this.size
+    );
+    this.asize += (gsize - this.asize) / 4;
+  }
+}
 
-	function init() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		for (let i = 0; i < 200; i++) {
-			particles.push(new Particle());
-		}
-	}
+function init() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < 200; i++) {
+    particles.push(new Particle());
+  }
+}
 
-	function loop() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+function loop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		mouse.x += (mouse.ex - mouse.x) / 4;
-		mouse.y += (mouse.ey - mouse.y) / 4;
+  mouse.x += (mouse.ex - mouse.x) / 4;
+  mouse.y += (mouse.ey - mouse.y) / 4;
 
-		const now = Date.now();
-		const delay = 1000;
-		if (
-			!mouse.down &&
-			now - mouse.inTime > delay &&
-			now - mouse.downTime > delay
-		) {
-			mouse.in = false;
-		}
+  const now = Date.now();
+  const delay = 1000;
+  if (
+    !mouse.down &&
+    now - mouse.inTime > delay &&
+    now - mouse.downTime > delay
+  ) {
+    mouse.in = false;
+  }
 
-		hoverDiv += ((mouse.down ? 3 : 8) - hoverDiv) / 4;
-		distMult = Math.min(window.innerWidth, window.innerHeight) / 4;
+  hoverDiv += ((mouse.down ? 3 : 8) - hoverDiv) / 4;
+  distMult = Math.min(window.innerWidth, window.innerHeight) / 4;
 
-		for (const particle of particles) {
-			particle.update();
-			particle.draw();
-		}
+  for (const particle of particles) {
+    particle.update();
+    particle.draw();
+  }
 
-		requestAnimationFrame(loop);
-	}
+  requestAnimationFrame(loop);
+}
 
-	init();
-	loop();
+init();
+loop();
