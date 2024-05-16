6c8cc27f014fd6f91c3c68030c54c1b5540fd3d1
fluxuat0r <163228692+fluxuat0r@users.noreply.github.com>
2024-05-16 02:18:23 +0000

    automatic push

--- scripts/home.js
+++ scripts/home.js
@@ -3,7 +3,7 @@
 // licensed under GNU Public License v3.0
 
 const canvas = document.getElementById("hero-canvas");
-canvas.width = document.clientWidth;
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 const particles = [];
